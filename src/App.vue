a62a69dee137662eaa51bc7e6329e47dbe54d3cf
marcofinucci <info@marcofinucci.com>
2024-01-25 09:28:13 +0100

    fix text

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ export default {
   <div class="container my-20 max-w-lg">
     <!-- Add new todo -->
     <div class="flex">
-      <input class="rounded" type="text" v-model="newTodo" placeholder="Nuovo todo" />
+      <input class="rounded" type="text" v-model="newTodo" placeholder="New todo" />
       <button class="ml-4 rounded bg-black px-6 text-white" @click="addTodo">Add</button>
     </div>
 
@@ -30,7 +30,7 @@ export default {
       <li class="mt-4 flex items-center rounded bg-slate-200 p-4" v-for="(todo, index) in todos" :key="index">
         <input class="rounded border-0" type="checkbox" v-model="todo.done" />
         <div class="ml-3 mr-8">{{ todo.text }}</div>
-        <button class="ml-auto text-red-500 underline decoration-1 underline-offset-2" @click="todos.splice(index, 1)">delete</button>
+        <button class="ml-auto text-red-500 underline decoration-1 underline-offset-2" @click="todos.splice(index, 1)">Delete</button>
       </li>
     </ul>
   </div>
